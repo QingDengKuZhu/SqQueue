d0bf0a729c8904707a333720227a28f21f42aef6
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-07-13 21:52:46 +0800

    实现初始化队列操作

--- SqQueue/Data_Base.c
+++ SqQueue/Data_Base.c
@@ -2,3 +2,23 @@
 #include "Main_First.h"
 #include "Data_Base.h"
 
+
+/*
+**初始化建立空对列时,令front = rear = 0.
+**每当插入新的队尾元素时,"尾指针增加1",
+**每当删除队头元素时,"头指针增加1",
+**因此,在非空队列中,头指针始终指向队列头元素,
+**而尾指针始终指向对列尾元素的下一个位置.
+*/
+void InitQueue(SQQUEUE *pQ)
+{
+	pQ->data = (Elem *)malloc(MAXQSIZE * sizeof(Elem));
+	if (!pQ->data)
+	{
+		printf("动态分配内存错误!\n");
+		exit(ERROR);
+	}
+	pQ->front = pQ->rear = 0;
+
+	return;
+}
