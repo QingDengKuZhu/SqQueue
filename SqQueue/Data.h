39bf7d6fbb8634b8a0b19c66749a0da3d6f10cea
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-07-13 21:35:15 +0800

    修改变量名

--- SqQueue/Data.h
+++ SqQueue/Data.h
@@ -9,7 +9,7 @@ typedef int Elem;
 
 typedef struct SqQueue
 {
-	Elem *sdata;	/*基地址,初始化的动态分配存储空间.*/
+	Elem *data;	/*基地址,初始化的动态分配存储空间.*/
 	int front;	/*存储队列头元素下标.*/
 	int rear;	/*若队列不为空,存储队尾元素的下一个位置的下标.*/
 }SQQUEUE;
