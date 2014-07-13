305828a127b548d4cec69e7d9f20294492a809d0
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-07-13 09:50:36 +0800

    定义数据结构

---
+++ Data.h
@@ -0,0 +1,17 @@
+﻿/*
+**循环队列的数据结构
+*/
+#ifndef DATA_H
+#define DATA_H
+
+#define MAXQSIZE	100	//最大队列长度
+typedef int Elem;
+
+typedef struct SqQueue
+{
+	Elem *qdata;	/*初始化的动态分配存储空间.*/
+	int front;	/*存储队列头元素下标.*/
+	int rear;	/*若队列不为空,存储队尾元素的下一个位置的下标.*/
+}SQQUEUE;
+
+#endif
